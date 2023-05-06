48ccb135cb1c413f17abe0f9bcdbe46f8546934e
fathima-1234 <fathimasuhra19@gmail.com>
2023-05-06 10:47:45 +0530

    changed

--- pillowmart/settings.py
+++ pillowmart/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-1@h4=e_rwsbfq8s1d8xsmaly9y*@0cl%_9vx-gfdurk(&hmo*b'
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY', default='my_default_secret_key')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = config('DEBUG',default=True , cast=bool)
